5ffc01e89024eb1bee064b36925cc0c0f077a0ed
dmytro-stasyuk <dmytro-stasyuk@users.noreply.github.com>
2026-06-18 09:09:43 +0000

    deploy: dmytro-stasyuk/spec-binder-website@77919dd0532aab62381e8ece5457686c49ed1f81

--- assets/js/814f3328.76e80a5d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkspec_binder_website=globalThis.webpackChunkspec_binder_website||[]).push([[7472],{5513(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Rethinking BDD automation with SpecBinder","permalink":"/blog/rethinking-bdd-automation","unlisted":false,"date":"2026-05-23T00:00:00.000Z"}]}')}}]);
